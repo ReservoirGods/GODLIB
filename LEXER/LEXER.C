de6769860eba5bd0f6d4322576414096ec2b64f3
pink-rg <leon_oreilly@yahoo.com>
2018-10-07 22:24:39 -0400

    LEXER - classify between 09az_AZ and other seperators. update unit test.

--- LEXER/LEXER.C
+++ LEXER/LEXER.C
@@ -1,39 +1,96 @@
+/*::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+::
+:: LEXER.C
+::
+:: Functions for tokenising strings based on whitespace divisions
+::
+:: [c] 2018 Reservoir Gods
+::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::*/
+
+
+/* ###################################################################################
+#  INCLUDES
+################################################################################### */
+
 #include	"LEXER.H"
 
+#include	<GODLIB\DEBUGLOG\DEBUGLOG.H>
 #include	<GODLIB\STRING\STRING.H>
+
+
+/* ###################################################################################
+#  CODE
+################################################################################### */
+
+/*-----------------------------------------------------------------------------------*
+* FUNCTION : Lexer_Init(sLexerContext * apContext, const sString * apString)
+* ACTION   : inits data structure. must be called before lexing
+* CREATION : 06.10.18 PNK
+*-----------------------------------------------------------------------------------*/
 
 void	Lexer_Init(sLexerContext * apContext, const sString * apString)
 {
 	apContext->mLineIndex = 0;
 	apContext->mOffset = 0;
 	apContext->mSource = *apString;
+	apContext->mSeperators[ 0 ] = 0;
+	apContext->mSeperators[ 1 ] = 0xFC00FF7AL;
+	apContext->mSeperators[ 2 ] = 0x78000001L;
+	apContext->mSeperators[ 3 ] = 0xF8000001L;
 	String_Init(&apContext->mToken, 0);
 }
+
+
+/*-----------------------------------------------------------------------------------*
+* FUNCTION : Lexer_Init2(sLexerContext * apContext, const char * apString)
+* ACTION   : inits data structure, absed on null terminated string must be called before lexing.
+* CREATION : 06.10.18 PNK
+*-----------------------------------------------------------------------------------*/
 
 void	Lexer_Init2(sLexerContext * apContext, const char * apString)
 {
 	apContext->mLineIndex = 0;
 	apContext->mOffset = 0;
+	apContext->mSeperators[ 0 ] = 0;
+	apContext->mSeperators[ 1 ] = 0xFC00FF7AL;
+	apContext->mSeperators[ 2 ] = 0x78000001L;
+	apContext->mSeperators[ 3 ] = 0xF8000001L;
 	String_Init( &apContext->mSource, apString);
 	String_Init( &apContext->mToken, 0);
 }
 
+
+/*-----------------------------------------------------------------------------------*
+* FUNCTION : Lexer_DeInit(sLexerContext * apContext)
+* ACTION   : ensures string deinit
+* CREATION : 06.10.18 PNK
+*-----------------------------------------------------------------------------------*/
+
 void	Lexer_DeInit(sLexerContext * apContext)
 {
+	String_DeInit( &apContext->mSource);
 	String_DeInit(&apContext->mToken);
 }
 
+
+/*-----------------------------------------------------------------------------------*
+* FUNCTION : U8	Lexer_GetNextToken( sLexerContext * apContext )
+* ACTION   : retrieves next token from buffer
+* CREATION : 06.10.18 PNK
+*-----------------------------------------------------------------------------------*/
+
 U8	Lexer_GetNextToken( sLexerContext * apContext )
 {
-	U8 lRes = 0;
 	U32 lOff;
+	U32 lMask;
 	U32 lLength = String_GetLength( &apContext->mSource );
 	char * lpChars = &apContext->mSource.mpChars[ apContext->mOffset ];
 	char * lpEnd = &apContext->mSource.mpChars[lLength ];
 
 	apContext->mTokenFlag = 0;
 
-	for (; lpChars < lpEnd && (' ' == *lpChars) || ('\t' == *lpChars) || (10 == *lpChars) || (13 == *lpChars); lpChars++)
+	/* skip whitespace before token */
+	for (; lpChars < lpEnd && (*lpChars<=' '); lpChars++)
 	{
 		if ((10 == *lpChars) || (13 == *lpChars))
 		{
@@ -43,29 +100,56 @@ U8	Lexer_GetNextToken( sLexerContext * apContext )
 		}
 	}
 
+	/* adjust token sto skip over whitespace */
 	apContext->mOffset = lpChars - apContext->mSource.mpChars;
 
-	for( ;lpChars<lpEnd; lpChars++ )
+	lMask   = 1;
+	lMask <<= (*lpChars & 31);
+
+	if( apContext->mSeperators[ *lpChars>>5 ] & lMask )
 	{
-		char c = *lpChars;
-		if( '\'' == c || '"' == c )
+		lpChars++;
+	}
+	else
+	{
+		/* read token until whitespace */
+		for( ;lpChars < lpEnd; lpChars++ )
 		{
-			while (lpChars < lpEnd && c != *++lpChars);
-			if (lpChars < lpEnd)
-				lpChars++;
+			char c = *lpChars;
+			if( '\'' == c || '"' == c )
+				while( lpChars < lpEnd && c != *++lpChars );
+			else if( c<=' ')
+				break;
+			else
+			{
+				lMask = 1;
+				lMask <<= (*lpChars & 31);
+				if( apContext->mSeperators[ *lpChars >> 5 ] & lMask )
+					break;
+			}
 		}
-		else if( ' ' == c || 9 == c || 10 ==c || 13 == c)
-			break;
 	}
 
+	/* absolute offset of token end*/
 	lOff = lpChars - apContext->mSource.mpChars;
 	if( lOff > apContext->mOffset )
 	{
+		/* set new token */
 		String_SetStatic( &apContext->mToken, &apContext->mSource.mpChars[ apContext->mOffset ], lOff - apContext->mOffset );
 		apContext->mOffset = lOff;
 		apContext->mTokenFlag = 1;
-		lRes = 1;
 	}
 
-	return( lRes );
+	return( (U8)apContext->mTokenFlag );
 }
+
+void	Lexer_SetSeperators( sLexerContext * apContext, const char * apSeps )
+{
+	while( *apSeps )
+	{
+		(void)apContext;
+	}
+}
+
+
+/* ################################################################################ */
--- LEXER/TEST/UT_LEXER.C
+++ LEXER/TEST/UT_LEXER.C
@@ -1,8 +1,16 @@
+/* ###################################################################################
+#  INCLUDES
+################################################################################### */
+
 #include	"..\LEXER.H"
 
 #include	<GODLIB\UNITTEST\UNITTEST.H>
 #include	<STDARG.H>
 
+
+/* ###################################################################################
+#  CODE
+################################################################################### */
 
 
 void Lexer_TokeniseValidate( const char * aString, ... )
@@ -13,13 +21,7 @@ void Lexer_TokeniseValidate( const char * aString, ... )
 	va_start(list,aString);
 
 	Lexer_Init2(&lContext, aString);
-/*
-	do
-	{
-		lpArg = va_arg(list, char*);
-		printf("lexer arg: %p\n", lpArg);
-	} while (lpArg);
-*/
+
 	while( Lexer_GetNextToken(&lContext))
 	{
 		sString stringCorrect;
@@ -30,7 +32,10 @@ void Lexer_TokeniseValidate( const char * aString, ... )
 
 		String_Init(&stringCorrect,0);
 		String_SetStaticNT(&stringCorrect, lpArg);
-/*		printf("correct: -%s-[%ld] token: -%s-[%ld]\n", stringCorrect.mpChars, String_GetLength(&stringCorrect), lContext.mToken.mpChars, String_GetLength(&lContext.mToken) );*/
+		if( !( String_IsEqual( &stringCorrect, &lContext.mToken ) ))
+		{
+			DebugLog_Printf4( "correct: -%s-[%ld] token: -%s-[%ld]\n", stringCorrect.mpChars, String_GetLength( &stringCorrect ), lContext.mToken.mpChars, String_GetLength( &lContext.mToken ) );
+		}
 		GOD_UNIT_TEST_EXPECT((String_IsEqual(&stringCorrect,&lContext.mToken)), "lexer tokenise mismatch");
 	}
 	if (!lContext.mTokenFlag)
@@ -48,7 +53,7 @@ void Lexer_TokeniseValidate( const char * aString, ... )
 
 GOD_UNIT_TEST( Lexer )
 {
-#if 1
+
 	Lexer_TokeniseValidate("", 0L);
 	Lexer_TokeniseValidate("single", "single", 0L);
 	Lexer_TokeniseValidate("two words", "two", "words", 0L);
@@ -58,10 +63,43 @@ GOD_UNIT_TEST( Lexer )
 	Lexer_TokeniseValidate(" some \"text in quote marks\" here", "some", "\"text in quote marks\"", "here", 0L);
 	Lexer_TokeniseValidate( "  other 'quote marks'", "other", "'quote marks'", 0L );
 	Lexer_TokeniseValidate( "  inside 'a quote \"in a quote\"'", "inside", "'a quote \"in a quote\"'", 0L );
-#endif
 	Lexer_TokeniseValidate( "line\nbreaks", "line", "breaks", 0L );
 	Lexer_TokeniseValidate( "another\rbreak", "another", "break", 0L );
-	Lexer_TokeniseValidate( "\tdo\tyou\tlike\ttabs?", "do", "you", "like", "tabs?", 0L );
-	Lexer_TokeniseValidate( " this   is 'some'\n\r\t mixed \n\"up stuff\"dont\tyou think?", "this", "is", "'some'", "mixed", "\"up stuff\"dont", "you", "think?", 0L );
+	Lexer_TokeniseValidate( "\tdo\tyou\tlike\ttabs?", "do", "you", "like", "tabs", "?", 0L );
+	Lexer_TokeniseValidate( " this   is 'some'\n\r\t mixed \n\"up stuff\"dont\tyou think?", "this", "is", "'some'", "mixed", "\"up stuff\"dont", "you", "think", "?", 0L );
+	Lexer_TokeniseValidate( "!@#$%",  "!", "@", "#", "$", "%", 0L );
 
+	/* validate all symbols */
+	{
+		const char *lpSymbolsAll = "!#$%&()*+,-./:;<=>?@[\\]^`{|}~";
+		const char *lpNonSymbols = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ_abcdefghijklmnopqrstuvwxyz";
+
+		const char * lpS = (const char*)lpSymbolsAll;
+		char * lp0 = "abc";
+		char * lp1 = "two bwords";
+		Lexer_TokeniseValidate( lp0, "abc", 0L );
+
+		for( ;*lpS; lpS++ )
+		{
+			char lChar[ 2 ] = { 0,0 };
+			lp0[ 1 ] = *lpS;
+			lChar[ 0 ] = *lpS;
+			Lexer_TokeniseValidate( lp0, "a", lChar, "c", 0L );
+			lp1[ 4 ] = *lpS;
+			Lexer_TokeniseValidate( lp1, "two", lChar, "words", 0L );
+		}
+
+		for( lpS=lpNonSymbols;*lpS; lpS++ )
+		{
+			char lChar[ 2 ] = { 0,0 };
+			lp0[ 1 ] = *lpS;
+			lChar[ 0 ] = *lpS;
+			Lexer_TokeniseValidate( lp0, lp0, 0L );
+			lp1[ 4 ] = *lpS;
+			Lexer_TokeniseValidate( lp1, "two", &lp1[4], 0L );
+		}
+	}
 }
+
+
+/* ################################################################################ */
