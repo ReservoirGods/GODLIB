ce678ed354fe65f6e4e8d4c4b5fb09210ccb154d
pink-rg <leon_oreilly@yahoo.com>
2018-10-07 22:27:52 -0400

    LEXER - unit tests for JSON

---
+++ LEXER/TEST/UT_JSON.C
@@ -0,0 +1,55 @@
+#include	"..\JSON.H"
+
+#include	<GODLIB\UNITTEST\UNITTEST.H>
+
+
+const char * gpJsonTest = "{}";
+
+void	JSONTest_NodeCount( const char * apString, U32 aCount )
+{
+	sString jsonString;
+	sNodeJSON * lpNode;
+	String_Init( &jsonString, apString );
+	lpNode = JSON_Parse( &jsonString );
+	GOD_UNIT_TEST_EXPECT( ( aCount == JSON_GetNodeCount( lpNode ) ), "json node count mismatch" );
+	JSON_Destroy( lpNode );
+}
+
+
+GOD_UNIT_TEST( JSON )
+{
+	sString jsonString;
+	sNodeJSON * lpNode;
+
+	JSONTest_NodeCount( "", 0 );
+	JSONTest_NodeCount( "{}", 1 );
+	JSONTest_NodeCount( "{ \"key\" }", 2 );
+	JSONTest_NodeCount( "{ \"key\" : \"value\" }", 3 );
+
+	String_Init( &jsonString, "{}" );
+	lpNode = JSON_Parse( &jsonString );
+	GOD_UNIT_TEST_EXPECT( ( 1 == JSON_GetNodeCount( lpNode )), "json node count mismatch" );
+	JSON_DebugShow( lpNode );
+	JSON_Destroy( lpNode );
+
+
+/*
+	String_Init( &jsonString, "{}" );
+	lpNode = JSON_Parse( &jsonString );
+	JSON_Destroy( lpNode );
+
+
+
+	String_Init( &jsonString, "{ \"value1\" : 12 }" );
+	lpNode = JSON_Parse( &jsonString );
+	JSON_Destroy( lpNode );
+	String_DeInit( &jsonString );
+*/
+//	String_Init( &jsonString, "{ \"value1\" : 12, \"value2\" : \"forty\", \"array\" : [ 6, 7, 9 ]  }" );
+	String_Init( &jsonString, "{ \"object1\" : { \"field0\" : \"value0\" }, \"object2\" : { \"field2\" : \"good\" } }" );
+//	String_Init( &jsonString, "{ \"objectArray\" : [ { \"field0\" : \"value0\" }, { \"field1\" : \"value1\" } ] }" );
+	lpNode = JSON_Parse( &jsonString );
+	JSON_DebugShow( lpNode );
+	JSON_Destroy( lpNode );
+	String_DeInit( &jsonString );
+}
