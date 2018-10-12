daed866b9c7944b7c26c2ed26e214f23ff5c69f1
pink-rg <leon_oreilly@yahoo.com>
2018-10-11 23:42:30 -0400

    DEBUGLOG - embed assembly code for steem test into C file as machine code

--- DEBUGLOG/DEBUGLOG.C
+++ DEBUGLOG/DEBUGLOG.C
@@ -123,6 +123,37 @@ U16		DebugLog_IsSTEEM( void )
 	return( 0 );
 }
 
+#else
+
+U16 	gpSteemCheck[] =
+{
+	0x48E7,0x7FFE,			/* movem.l d1-a6,-(a7) */
+	0x2C3C,0x456D,0x753F,	/* move.l #'Emu?',d6*/
+	0x2E06,					/* move.l d6,d7*/
+	0x3F3C,0x0025,			/* move.l #$25,-(a7)*/
+	0x4E4E,					/* trap 14 */
+	0x548F,					/* addq.l #2,a7 */
+	0x7000,					/* moveq #0,d0 */
+	0xBCBC,0x5354,0x4565,	/* cmp.l #'STEe',d6 */
+	0x660A,					/* bne.s .not*/
+	0xBEBC,0x6D45,0x6E67,	/* cmp.l #'mEng',d6 */
+	0x6602,					/* bne.s .not */
+	0x7001,					/* moveq #1,d0 */
+	0x4CDF,0x7FFE,			/* move.l (a7)+,d1-a6 */
+	0x4e75					/* RTS */
+};
+
+typedef U16	(*fIsSteem )( void );
+
+U16		DebugLog_IsSTEEM( void )
+{
+	/* C doesn't like us converting data pointers to function pointers, so let's hack around this */
+	fIsSteem lFunc;
+	U16 ** lppF = (U16**)&lFunc;
+	*lppF = gpSteemCheck;
+
+	return( lFunc() );
+}
 #endif // !dGODLIB_ATARI
 
 
